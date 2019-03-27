f27b5c52202b09b12ef14b7b0a93c2dd5c7bb930
West Kraemer <kraemerwest@gmail.com>
2019-03-27 15:02:00 -0400

    dd

--- appEventCount/AppDelegate.swift
+++ appEventCount/AppDelegate.swift
@@ -12,31 +12,44 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var viewController: ViewController?
 
 //this
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        viewController = window?.rootViewController as? ViewController
+        viewController?.didFinishLaunchingCOUNT += 1
         print("did finish launching")
         return true
     }
 //this
     func applicationWillResignActive(_ application: UIApplication) {
-       print("will resign active")
+        viewController = window?.rootViewController as? ViewController
+        viewController?.willResignActiveCOUNT += 1
+        print("will resign active")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-       print("did enter background")
+        viewController = window?.rootViewController as? ViewController
+        viewController?.didEnterBackgroundCOUNT += 1
+        print("did enter background")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-      print("will enter foreground")
+        viewController = window?.rootViewController as? ViewController
+        viewController?.willEnterForegroundCOUNT += 1
+        print("will enter foreground")
     }
 //and this are the ones you want to focus on at the now
     func applicationDidBecomeActive(_ application: UIApplication) {
-     print("did becomes active")
+        viewController = window?.rootViewController as? ViewController
+        viewController?.didBecomeActiveCOUNT += 1
+        print("did becomes active")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-      print("will terminate")
+        viewController = window?.rootViewController as? ViewController
+        viewController?.willTerminateCOUNT += 1
+        print("will terminate")
     }
 
 
--- appEventCount/ViewController.swift
+++ appEventCount/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateView()
     }
     
     func updateView() {
